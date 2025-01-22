a27d5a06b1b50e4d1897c535c03e7c6cb3519553
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2025-01-22 23:19:02 +0530

    StockSimplify project start

---
+++ StockSimplify/StockSimplify-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 rounded-md m-1 text-center">StockSimplify</h1>
+    </>
+  );
+}
+
+export default App;
