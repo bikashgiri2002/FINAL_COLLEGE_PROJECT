ea54a4faa4ff739305a6e801903ef75cd94e7ca3
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2025-01-23 22:09:02 +0530

    backend db connection

--- StockSimplify/StockSimplify-backend/server.js
+++ StockSimplify/StockSimplify-backend/server.js
@@ -1,1 +1,16 @@
-console.log("StockSimplify backend started");
+const express = require("express");
+require('dotenv').config();
+
+// create express app
+const app = express();
+
+// port numbet from .env variable
+const port = process.env.PORT || 3000;
+
+// import dp connections
+require("./config/db");
+
+// server listening on port
+app.listen(port, () => {
+    console.log(`server listen on port ${port}`);
+  });
