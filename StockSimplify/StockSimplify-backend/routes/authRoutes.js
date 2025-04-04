a4a02218673596a140c82aff5273e80c1ccfafc3
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2025-04-04 23:45:10 +0530

    stockmete new project

--- StockSimplify/StockSimplify-backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
-
-module.exports = router;
--- StockSimplify/StockSimplify-backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-// Route to add a new product
-router.post('/add-product', authMiddleware, productController.addProduct);
-
-module.exports = router;
