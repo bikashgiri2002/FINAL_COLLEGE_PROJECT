54deaf5e35a242b567c7547998ca8341bf371460
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2025-04-19 15:31:11 +0530

    nav bar log in button and register button size

--- StockMateOwnProject/frontend/src/components/Navbar.jsx
+++ StockMateOwnProject/frontend/src/components/Navbar.jsx
@@ -167,14 +167,14 @@ const Navbar = () => {
               ))}
               <NavLink
                 to="/login"
-                className="block bg-blue-400 text-white px-4 py-2 rounded-md mx-4 dark:bg-blue-600 dark:hover:bg-blue-700"
+                className="block bg-blue-400 text-white px-4 py-2 rounded-md mx-4 dark:bg-blue-600 dark:hover:bg-blue-700 w-20"
                 onClick={() => setIsMobileMenuOpen(false)}
               >
                 Login
               </NavLink>
               <NavLink
                 to="/register"
-                className="block bg-blue-400 text-white px-4 py-2 rounded-md mx-4 dark:bg-blue-600 dark:hover:bg-blue-700"
+                className="block bg-blue-400 text-white px-4 py-2 rounded-md mx-4 dark:bg-blue-600 dark:hover:bg-blue-700 w-20"
                 onClick={() => setIsMobileMenuOpen(false)}
               >
                 Register
